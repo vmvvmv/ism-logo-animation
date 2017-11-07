107bddb17fde7bc76ab866b1374b89809872e6f0
Unknown <vmvvmv99@gmail.com>
2017-11-07 12:04:24 +0200

    Glow after assembly, remove line change color before assembly.

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ var container = $('#container');
 
 for( obj of ismLogos ) {
 
-	container.append('<svg id="'+obj.id+'" class="' + 'ism-logo' + '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1000 90" width="100%" height="100%" preserveAspectRatio="none"> <polyline id="ISM-M" vector-effect="non-scaling-stroke" points="377.5 87 377.5 6 683.5 87 994 6 994.5 87" /> <path id="ISM-S" vector-effect="non-scaling-stroke" d="M22.5,6c30.87,54.24,61.74,81,97,81,30,0,47.34-16.88,73-42.5,28.29-28.29,52.81-38.26,75-38.5,33.18-.36,66,27.53,99.5,81"> </path> <polyline id="ISM-I" vector-effect="non-scaling-stroke" points="5.5 87 5.5 45.5 5.5 6" /> </svg> ');
+	container.append('<svg id="'+obj.id+'" class="' + 'ism-logo' + '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1000 90" width="100%" height="100%" preserveAspectRatio="none" shape-rendering="geometricPrecision"> <polyline id="ISM-M" shape-rendering="geometricPrecision" vector-effect="non-scaling-stroke" points="377.5 87 377.5 6 683.5 87 994 6 994.5 87" /> <path id="ISM-S" vector-effect="non-scaling-stroke" d="M22.5,6c30.87,54.24,61.74,81,97,81,30,0,47.34-16.88,73-42.5,28.29-28.29,52.81-38.26,75-38.5,33.18-.36,66,27.53,99.5,81"> </path> <polyline id="ISM-I" vector-effect="non-scaling-stroke" points="5.5 87 5.5 45.5 5.5 6" /> </svg> ');
 
 }
 
@@ -93,7 +93,6 @@ function initISMLogo() {
 	ismLogo.expand = function (t) {
 
 		var times = new Array( ismLogos.length ).fill(0);
-
 		times[0] = t;
 
 		if (!t) {
@@ -102,11 +101,7 @@ function initISMLogo() {
 
 		for ( index in times ) {
 
-			if(index % 2 === 0)
-				times[index] = times[0] + times[index] * 0.2;
-			else
-				times[index] = times[0] - times[index] * 0.2;
-
+			times[index] = t;
 
 		}
 
@@ -123,58 +118,47 @@ function initISMLogo() {
 				opacity: 0.2,
 				transform: 's1,1 t0,0',
 				// 'stroke-dashoffset':v,
-				// 'stroke-dasharray':v
 			}, times[i] * 1.2, mina.easein);
 			window[obj.key].M2.animate({
 				strokeWidth: 4,
 				opacity: 0.8,
 				transform: 's1,1 t0,0',
-				// 'stroke-dashoffset':v,
-				// 'stroke-dasharray':v
+
 			}, times[i] * 1.3, mina.easein);
 			window[obj.key].M3.animate({
 				strokeWidth: 4,
 				opacity: 0.2,
 				transform: 's1,1 t0,0',
-				// 'stroke-dashoffset':v,
-				// 'stroke-dasharray':v
+
 			}, times[i] * 1.3, mina.easein);
 			window[obj.key].S.animate({
 				strokeWidth: 4,
 				opacity: 0.8,
 				transform: 's1,1 t0,0',
-				// 'stroke-dashoffset':v,
-				// 'stroke-dasharray':v
+
 			}, times[i] * 1.4, mina.easein, function () {});
 			window[obj.key].S2.animate({
 				strokeWidth: 4,
 				opacity: 0.8,
 				transform: 's1,1 t0,0',
-				// 'stroke-dashoffset':v,
-				// 'stroke-dasharray':v
+
 			}, times[i] * 1.2, mina.easein, function () {});
 			window[obj.key].S3.animate({
 				strokeWidth: 4,
 				opacity: 0.8,
 				transform: 's1,1 t0,0',
-				// 'stroke-dashoffset':v,
-				// 'stroke-dasharray':v
+
 			}, times[i] * 1.4, mina.easein, function () {});
 			window[obj.key].I.animate({
 				strokeWidth: 4,
 				opacity: 0.8,
 				transform: 's1,1 t0,0',
-				// 'stroke-dashoffset':v,
-				// 'stroke-dasharray':v
 			}, times[i] * 1.2, mina.easein, function () {});
 
 			i++;
 		}
 
-		var f = window['ismLogo'].s.filter(Snap.filter.blur(1, 1));
-
-		//var shadow =  window['ismLogo'].s.filter(Snap.filter.shadow(0, 2, 3));
-		var filterChild = f.node.firstChild;
+	
 
 		Snap.animate(5000, 0, function (value) {
 
@@ -188,27 +172,9 @@ function initISMLogo() {
 				var v = {
 					'stroke-dashoffset':0,
 					'stroke-dasharray':0,
-					//opacity:1,
-					stroke: 'white'
-					//filter:f
+	
 				}
-				Snap.animate( 30, 0, function( v ) { 
-					filterChild.attributes[0].value = v + ',' + v;  
-					var v2 = {
-						filter:f,
-						strokeWidth: v  / 2
-					}
-
-					window['ismLogo'].I.attr(v2);
-					window['ismLogo'].S.attr(v2);
-					window['ismLogo'].S2.attr(v2);
-					window['ismLogo'].S3.attr(v2);
-					window['ismLogo'].M.attr(v2);
-					window['ismLogo'].M2.attr(v2);
-					window['ismLogo'].M3.attr(v2);
-
-				}, 2500 * 1.5, mina.easeinout );
-									
+						
 			}
 
 			for( obj of ismLogos ) { 
@@ -228,7 +194,56 @@ function initISMLogo() {
 		
 	}
 
+	ismLogo.glow = function () {
 
+		var f = window['ismLogo'].s.filter(Snap.filter.blur(1, 1));
+		
+		var filterChild = f.node.firstChild;
+
+		var v = {
+			'stroke-dashoffset':0,
+			'stroke-dasharray':0,
+			stroke: 'white'
+		}
+
+							
+	
+		for( obj of ismLogos ) { 
+
+			window[obj.key].I.attr(v);
+			window[obj.key].S.attr(v);
+			window[obj.key].S2.attr(v);
+			window[obj.key].S3.attr(v);
+			window[obj.key].M.attr(v);
+			window[obj.key].M2.attr(v);
+			window[obj.key].M3.attr(v);
+
+			window['ismLogo'].I = window[obj.key].I;
+
+		}	
+
+		
+
+		Snap.animate( 10, 0, function( v ) { 
+			filterChild.attributes[0].value = v + ',' + v;  
+			var v2 = {
+				filter:f,
+				strokeWidth: v,
+				stroke: 'white',
+				opacity: 1
+			}
+
+			window['ismLogo'].I.attr(v2);
+			window['ismLogo'].S.attr(v2);
+			window['ismLogo'].S2.attr(v2);
+			window['ismLogo'].S3.attr(v2);
+			window['ismLogo'].M.attr(v2);
+			window['ismLogo'].M2.attr(v2);
+			window['ismLogo'].M3.attr(v2);
+
+		}, 2500 * 1.5, mina.easeinout );
+
+	}
 
 	ismLogo.init = function () {
 
@@ -236,6 +251,10 @@ function initISMLogo() {
 		setTimeout(function () {
 			ismLogo.expand(2300)
 		}, 700);
+
+		setTimeout(function () {
+			ismLogo.glow()
+		}, 5000);
 
 	}
 
